1b5ebb133c9f33fd9ced128f761eb2f46c47fc96
Kokurin Egor <kokurinegorr@gmail.com>
2022-11-25 21:50:00 +0300

    Добавление фрукта и его генерация; добавление тела змейки, если она съела фрукт; добавление счетчика очков; редактирование управления; добавление условия - если змейка выйдет за границы карты, то она выйдет с другой стороны; добавление окна поражения, если змейка врежиться в свое тело; добавление в это окно кнопки рестарта

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -28,37 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
 			this.SuspendLayout();
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-			this.pictureBox1.Location = new System.Drawing.Point(100, 60);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(20, 20);
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(334, 312);
-			this.Controls.Add(this.pictureBox1);
 			this.KeyPreview = true;
 			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
 			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
 
         }
 
         #endregion
 
-		private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -12,40 +12,152 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
+		private int dvX;
+		private int dvY;
         private int loc_x;
         private int loc_y;
         private PictureBox fruit;
+		private PictureBox[] body = new PictureBox[10000]; //размер змейки
+        private Label Score;
         private int map_width = 200;
         private int map_height = 200;
+		private int y;
         private int size = 20;
         public Form1()
         {
             InitializeComponent();
-            //PictureBox snake = new PictureBox();
-            //snake.Size = new Size(20,20);
-            //snake.Location = new Point(20,20);
-            //snake.BackColor = Color.Green;
-            //this.Controls.Add(snake);
-			//MessageBox.Show(e.KeyCode.ToString());
-			//MessageBox.Show(e.KeyChar.ToString());
-
+            body[0] = new PictureBox();
+            body[0].Size = new Size(size, size);
+            body[0].BackColor = Color.Red;
+            body[0].Location = new Point(0, 0);
+            this.Controls.Add(body[0]);
+            y = 0;
+			dvX = 1;
+			dvY = 0;
+            Score = new Label();
+            Score.Location = new Point(220, 10);
+            Score.Size = new Size(100, 20);
+            Score.ForeColor = Color.Black;
+            Score.Font = new Font("Times New Roman", 12, FontStyle.Bold);
+            Score.Text = "Score: 0";
+            this.Controls.Add(Score);
             this.Controls.Add(fruit);
-           // this.map_width = map_width;
-           // this.map_height = map_height;
             Map();
             fruit = new PictureBox();
             fruit.BackColor = Color.Yellow;
-            fruit.Size = new Size(20, 20);
+			fruit.Size = new Size(size, size);
             this.Controls.Add(fruit);
             Loc_fruit();
-            //if(pictureBox1.Location == fruit.Location)
-            //{
-            //    MessageBox.Show("Ну здарова");
-            //}
+
+			timer1.Tick += new EventHandler(upp);
+			timer1.Interval = 200;
+			timer1.Start();
         }
+        private void upp(Object myObject, EventArgs e)
+        {
+            eat_fruit();
+
+            for (int i = y; i >= 1; --i)
+            {
+                body[i].Location = body[i - 1].Location;
+            }
+            body[0].Location = new Point(body[0].Location.X + dvX * (size), body[0].Location.Y + dvY * (size));
+
+			#region Появление змейки с другой стороны поля
+			if (body[0].Location.X >= (map_width))
+            {
+                body[0].Location = new Point(0, body[0].Location.Y + dvY * (size));
+            }
+           if (body[0].Location.X < 0)
+           {
+               body[0].Location = new Point(180, body[0].Location.Y + dvY * (size));
+           }
+            if (body[0].Location.Y >= (map_height))
+            {
+                body[0].Location = new Point(body[0].Location.X + dvX * (size), 0);
+            }
+            if (body[0].Location.Y < 0)
+            {
+                body[0].Location = new Point(body[0].Location.X + dvX * (size), 180);
+			}
+			#endregion
+
+			#region Если мы врезаемся в тело змейки
+			for (int i = 1; i <= y; ++i)
+            {
+               
+                if (body[0].Location == body[i].Location)
+                {
+                    Controls.Clear();
+                    Label died = new Label();
+                    died.Font = new Font("Times New Roman", 20, FontStyle.Bold);
+                    died.ForeColor = Color.Black;
+                    died.Size = new Size(150, 40);
+                    died.Location = new Point(100, 50);
+                    died.Text = "You Died!";
+                    this.Controls.Add(died);
+
+                    Label record = new Label();
+                    record.Font = new Font("Times New Roman", 20, FontStyle.Bold);
+                    record.ForeColor = Color.Black;
+                    record.Size = new Size(200, 40);
+                    record.Location = new Point(110, 100);
+                    record.Text = "Score: " + y;
+                    this.Controls.Add(record);
+
+                    Button start = new Button();
+                    start.Size = new Size(150,40);
+                    start.Font = new Font("Times New Roman", 12, FontStyle.Bold);
+                    start.ForeColor = Color.Black;
+                    start.Text = "Начать заново";
+                    start.Location = new Point(90, 170);
+					start.Click += new EventHandler(Start);
+                    this.Controls.Add(start);
+
+
+                    Button stop = new Button();
+                    stop.Size = new Size(150, 40);
+                    stop.Font = new Font("Times New Roman", 12, FontStyle.Bold);
+                    stop.ForeColor = Color.Black;
+                    stop.Text = "Вернуться в меню";
+                    stop.Location = new Point(90, 220);
+                    this.Controls.Add(stop);
+                }
+
+			}
+			#endregion
+
+		}
+		public void Start(object sender, EventArgs e)
+		{
+			Application.Restart();
+		}
+        static void start_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+        //Если съели фрукт
+   private void eat_fruit()
+        {
+             if(body[0].Location == fruit.Location)     //сравниваем координаты фрукта и головы змеи
+            {
+                ++y;                                    //если равн, то увеличиваем наши очки - y;
+                Score.Text = "Score: " + y;             //выводим очки
+
+                body[y] = new PictureBox();
+                body[y].Size = new Size(size, size);
+                body[y].BackColor = Color.Green;
+                body[y].Location = new Point(body[y - 1].Location.X + dvX * size, body[y - 1].Location.Y + dvY * size);
+                this.Controls.Add(body[y]);
+               
+                Loc_fruit(); //генерируем новое положение фрукта
+            }
+        }
+
+        //Генерация фрукта
         private void Loc_fruit()
         {
-
+            m_start:
             Random rnd = new Random();
             loc_x = rnd.Next(0, map_width);
             int X = loc_x % size;
@@ -56,7 +168,19 @@ namespace Snake
             loc_y = loc_y - Y;
            
             fruit.Location = new Point(loc_x, loc_y);
-        }
+
+            #region Чтобы фрукт не генерировался в змее
+            for (int i = 0; i <= y; ++i )
+            {
+                if (fruit.Location == body[i].Location || fruit.Location == body[0].Location)
+                {
+                    goto m_start;
+                }
+            }
+            #endregion
+        } 
+
+        //Генераци карты
         private void Map()
         {
             for (int i = 1; i <= map_width / size; ++i)
@@ -67,7 +191,7 @@ namespace Snake
                 pic.Size = new Size(map_width, 1);
                 this.Controls.Add(pic);
             }
-            for(int i =1; i <= map_width / size ; ++i)
+            for (int i = 1; i <= map_width / size; ++i)
             {
                 PictureBox pic = new PictureBox();
                 pic.BackColor = Color.Black;
@@ -77,54 +201,61 @@ namespace Snake
             }
 
         }
-     
-       
-
-
+    
         # region Управление
-        private void button1_Click(object sender, EventArgs e)
-        {
-            pictureBox1.Location = new Point(pictureBox1.Location.X + 20, pictureBox1.Location.Y);
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            pictureBox1.Location = new Point(pictureBox1.Location.X - 20, pictureBox1.Location.Y);
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 20);
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 20);
-        }
-        #endregion
-
-
 		private void Form1_KeyDown(object sender, KeyEventArgs e)
 		{
-			if(e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+			if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
 			{
-				pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 20);
+				if(dvY == 1)
+				{
+				}
+				else
+				{
+					dvY = -1;
+					dvX = 0;
+				}
 			}
 			if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
 			{
-				pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 20);
+				if(dvY == -1)
+				{
+				}
+				else
+				{
+					dvY = 1;
+					dvX = 0;	
+				}
 			}
 			if (e.KeyCode == Keys.D || e.KeyCode == Keys.Right)
 			{
-				pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 20);
+				if(dvX == -1)
+				{
+				}
+				else
+				{
+					dvX = 1;
+					dvY = 0;
+				}
 			}
 			if (e.KeyCode == Keys.A || e.KeyCode == Keys.Left)
 			{
-				pictureBox1.Location = new Point(pictureBox1.Location.X - 20, pictureBox1.Location.Y);
+				if (dvX == 1)
+				{
+				}
+				else
+				{
+					dvX = -1;
+					dvY = 0;
+				}
 			}
 		}
+        #endregion
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
-		
+        }
+	
 	}
 }
